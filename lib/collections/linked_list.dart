db1632ca846a3f5c8cf41cbe95479cc0103eb96b
Jaider Andres Pinto Pinto <jaider.pinto@hotmail.com>
2020-10-04 21:07:09 -0500

    LinkedList agregada

--- lib/collections/linked_list.dart
+++ lib/collections/linked_list.dart
@@ -1,3 +1,80 @@
-class LinkedList{
-  
+import 'package:ed_project/collections/node.dart';
+
+class LinkedList {
+  Node head;
+  Node tail;
+  int size;
+
+  LinkedList() {
+    head = null;
+    size = 0;
+    tail = null;
+  }
+
+  void pushFront(dynamic key) {
+    if (head == null) {
+      head = Node(key);
+      tail = Node(key);
+    } else {
+      final Node n = Node(key);
+      n.nextValue = head;
+      head = n;
+    }
+    size++;
+  }
+
+  dynamic topFront() {
+    return head.value;
+  }
+
+  void popFront() {
+    if (size == 0) return;
+    head = head.nextValue;
+    size--;
+  }
+
+  void pushBack(dynamic key) {
+    if (head == null) {
+      head = Node(key);
+      tail = Node(key);
+    } else {
+      final Node n = Node(key);
+      tail.nextValue = n;
+      tail = n;
+    }
+    size++;
+  }
+
+  dynamic topBack() {
+    return tail.value;
+  }
+
+  void popBack() {
+    if (size == 0) return;
+    if (size == 1) {
+      head = null;
+      tail = null;
+    } else {
+      Node n = head;
+      while (n.nextValue.nextValue != null) {
+        n = n.nextValue;
+      }
+      n.nextValue = null;
+      tail = n;
+    }
+    size--;
+  }
+
+  bool find(dynamic key) {
+    Node n = head;
+    bool ans = false;
+    while (n != null) {
+      if (n.value == key) {
+        ans = true;
+        break;
+      }
+      n = n.nextValue;
+    }
+    return ans;
+  }
 }
--- lib/collections/node.dart
+++ lib/collections/node.dart
@@ -2,5 +2,9 @@ class Node {
   dynamic value;
   Node nextValue;
 
-  Node({this.value, this.nextValue});
+  Node(dynamic val) {
+    // ignore: prefer_initializing_formals
+    value = val;
+    nextValue = null;
+  }
 }
