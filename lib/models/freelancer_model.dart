26e3650a27df96ba0ca93cd025381338615fc35e
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-10-11 19:03:36 -0500

    update models

--- lib/models/freelancer_model.dart
+++ lib/models/freelancer_model.dart
@@ -1,84 +1,106 @@
+// To parse this JSON data, do
+//
+//     final freelancerModel = freelancerModelFromJson(jsonString);
+
 import 'dart:convert';
 
-List<LessonsModel> lessonsModelFromJson(String str) => List<LessonsModel>.from(
-    json.decode(str).map((x) => LessonsModel.fromJson(x)));
+List<FreelancerModel> freelancerModelFromJson(String str) => List<FreelancerModel>.from(json.decode(str).map((x) => FreelancerModel.fromJson(x)));
 
-String lessonsModelToJson(List<LessonsModel> data) =>
-    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String freelancerModelToJson(List<FreelancerModel> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
-class LessonsModel {
-  LessonsModel({
-    this.name,
-    this.lastName,
-    this.username,
-    this.location,
-    this.gender,
-    this.avatarUrl,
-    this.services,
-  });
+class FreelancerModel {
+    FreelancerModel({
+        this.name,
+        this.lastName,
+        this.username,
+        this.location,
+        this.gender,
+        this.avatarUrl,
+        this.services,
+    });
 
-  String name;
-  String lastName;
-  String username;
-  String location;
-  String gender;
-  String avatarUrl;
-  List<Service> services;
+    String name;
+    String lastName;
+    String username;
+    String location;
+    Gender gender;
+    String avatarUrl;
+    List<Service> services;
 
-  factory LessonsModel.fromJson(Map<String, dynamic> json) => LessonsModel(
+    factory FreelancerModel.fromJson(Map<String, dynamic> json) => FreelancerModel(
         name: json["name"],
         lastName: json["last_name"],
         username: json["username"],
         location: json["location"],
-        gender: json["gender"],
+        gender: genderValues.map[json["gender"]],
         avatarUrl: json["avatar_url"],
-        services: List<Service>.from(
-            json["services"].map((x) => Service.fromJson(x))),
-      );
+        services: List<Service>.from(json["services"].map((x) => Service.fromJson(x))),
+    );
 
-  Map<String, dynamic> toJson() => {
+    Map<String, dynamic> toJson() => {
         "name": name,
         "last_name": lastName,
         "username": username,
         "location": location,
-        "gender": gender,
+        "gender": genderValues.reverse[gender],
         "avatar_url": avatarUrl,
         "services": List<dynamic>.from(services.map((x) => x.toJson())),
-      };
+    };
 }
 
+enum Gender { FEMALE, MALE }
+
+final genderValues = EnumValues({
+    "female": Gender.FEMALE,
+    "male": Gender.MALE
+});
+
 class Service {
-  Service({
-    this.serviceName,
-    this.date,
-    this.category,
-    this.imageUrl,
-    this.numberStars,
-    this.price,
-  });
+    Service({
+        this.serviceName,
+        this.date,
+        this.category,
+        this.imageUrl,
+        this.numberStars,
+        this.price,
+    });
 
-  String serviceName;
-  DateTime date;
-  String category;
-  String imageUrl;
-  int numberStars;
-  int price;
+    String serviceName;
+    DateTime date;
+    String category;
+    String imageUrl;
+    int numberStars;
+    int price;
 
-  factory Service.fromJson(Map<String, dynamic> json) => Service(
+    factory Service.fromJson(Map<String, dynamic> json) => Service(
         serviceName: json["service_name"],
         date: DateTime.parse(json["date"]),
         category: json["category"],
         imageUrl: json["image_url"],
         numberStars: json["numberStars"],
         price: json["price"],
-      );
+    );
 
-  Map<String, dynamic> toJson() => {
+    Map<String, dynamic> toJson() => {
         "service_name": serviceName,
         "date": date.toIso8601String(),
         "category": category,
         "image_url": imageUrl,
         "numberStars": numberStars,
         "price": price,
-      };
+    };
+}
+
+class EnumValues<T> {
+    Map<String, T> map;
+    Map<T, String> reverseMap;
+
+    EnumValues(this.map);
+
+    Map<T, String> get reverse {
+        if (reverseMap == null) {
+            reverseMap = map.map((k, v) => new MapEntry(v, k));
+        }
+        return reverseMap;
+    }
 }
--- lib/models/profile_model.dart
+++ lib/models/profile_model.dart
@@ -1,93 +1,118 @@
 // To parse this JSON data, do
 //
-//     final lessonsModel = lessonsModelFromJson(jsonString);
+//     final freelancerModel = freelancerModelFromJson(jsonString);
 
 import 'dart:convert';
 
-List<LessonsModel> lessonsModelFromJson(String str) => List<LessonsModel>.from(json.decode(str).map((x) => LessonsModel.fromJson(x)));
+List<FreelancerModel> freelancerModelFromJson(String str) =>
+    List<FreelancerModel>.from(
+        json.decode(str).map((x) => FreelancerModel.fromJson(x)));
 
-String lessonsModelToJson(List<LessonsModel> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String freelancerModelToJson(List<FreelancerModel> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
-class LessonsModel {
-    LessonsModel({
-        this.name,
-        this.lastName,
-        this.username,
-        this.email,
-        this.password,
-        this.location,
-        this.gender,
-        this.avatarUrl,
-        this.lastTransactions,
-    });
+class FreelancerModel {
+  FreelancerModel({
+    this.name,
+    this.lastName,
+    this.username,
+    this.email,
+    this.password,
+    this.location,
+    this.gender,
+    this.avatarUrl,
+    this.lastTransactions,
+  });
 
-    String name;
-    String lastName;
-    String username;
-    String email;
-    String password;
-    String location;
-    String gender;
-    String avatarUrl;
-    List<LastTransaction> lastTransactions;
+  String name;
+  String lastName;
+  String username;
+  String email;
+  String password;
+  String location;
+  Gender gender;
+  String avatarUrl;
+  List<LastTransaction> lastTransactions;
 
-    factory LessonsModel.fromJson(Map<String, dynamic> json) => LessonsModel(
+  factory FreelancerModel.fromJson(Map<String, dynamic> json) =>
+      FreelancerModel(
         name: json["name"],
         lastName: json["last_name"],
         username: json["username"],
         email: json["email"],
         password: json["password"],
         location: json["location"],
-        gender: json["gender"],
+        gender: genderValues.map[json["gender"]],
         avatarUrl: json["avatar_url"],
-        lastTransactions: List<LastTransaction>.from(json["last_transactions"].map((x) => LastTransaction.fromJson(x))),
-    );
+        lastTransactions: List<LastTransaction>.from(
+            json["last_transactions"].map((x) => LastTransaction.fromJson(x))),
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "name": name,
         "last_name": lastName,
         "username": username,
         "email": email,
         "password": password,
         "location": location,
-        "gender": gender,
+        "gender": genderValues.reverse[gender],
         "avatar_url": avatarUrl,
-        "last_transactions": List<dynamic>.from(lastTransactions.map((x) => x.toJson())),
-    };
+        "last_transactions":
+            List<dynamic>.from(lastTransactions.map((x) => x.toJson())),
+      };
 }
 
+enum Gender { FEMALE, MALE }
+
+final genderValues = EnumValues({"female": Gender.FEMALE, "male": Gender.MALE});
+
 class LastTransaction {
-    LastTransaction({
-        this.freelancer,
-        this.serviceName,
-        this.description,
-        this.date,
-        this.category,
-        this.price,
-    });
+  LastTransaction({
+    this.freelancer,
+    this.serviceName,
+    this.description,
+    this.date,
+    this.category,
+    this.price,
+  });
 
-    String freelancer;
-    String serviceName;
-    String description;
-    DateTime date;
-    String category;
-    int price;
+  String freelancer;
+  String serviceName;
+  String description;
+  DateTime date;
+  String category;
+  int price;
 
-    factory LastTransaction.fromJson(Map<String, dynamic> json) => LastTransaction(
+  factory LastTransaction.fromJson(Map<String, dynamic> json) =>
+      LastTransaction(
         freelancer: json["freelancer"],
         serviceName: json["service_name"],
         description: json["description"],
         date: DateTime.parse(json["date"]),
         category: json["category"],
         price: json["price"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "freelancer": freelancer,
         "service_name": serviceName,
         "description": description,
         "date": date.toIso8601String(),
         "category": category,
         "price": price,
-    };
+      };
+}
+
+class EnumValues<T> {
+  Map<String, T> map;
+  Map<T, String> reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    if (reverseMap == null) {
+      reverseMap = map.map((k, v) => new MapEntry(v, k));
+    }
+    return reverseMap;
+  }
 }
