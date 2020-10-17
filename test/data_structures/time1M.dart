ad4f8490755e7e8c2b06faf4c7535ab5177f4f85
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-10-16 21:44:25 -0500

    set test time to miliseconds

--- test/data_structures/time1M.dart
+++ test/data_structures/time1M.dart
@@ -9,13 +9,14 @@ void main() {
       for (int i = 0; i < 1000000; i++) {
         lista.pushBack(i);
       }
-      print('PushBack para 1M de datos en ${stopwatch.elapsed}');
+      print('PushBack para 1M de datos en ${stopwatch.elapsedMilliseconds}');
       expect(true, true);
     });
     test("PopBack", () {
       Stopwatch stopwatch = new Stopwatch()..start();
       lista.popBack();
-      print('PopBack en una lista de 1M de datos en ${stopwatch.elapsed}');
+      print(
+          'PopBack en una lista de 1M de datos en ${stopwatch.elapsedMilliseconds}');
       lista.pushBack(999999);
       expect(true, true);
     });
@@ -23,63 +24,63 @@ void main() {
       Stopwatch stopwatch = new Stopwatch()..start();
       lista.find(0);
       print(
-          'Find para un elemento al inicio de una lista de 1M de datos en ${stopwatch.elapsed}');
+          'Find para un elemento al inicio de una lista de 1M de datos en ${stopwatch.elapsedMilliseconds}');
       expect(true, true);
     });
     test("find 499999", () {
       Stopwatch stopwatch = new Stopwatch()..start();
       lista.find(499999);
       print(
-          'Find para un elemento en el centro de una lista de 1M de datos en ${stopwatch.elapsed}');
+          'Find para un elemento en el centro de una lista de 1M de datos en ${stopwatch.elapsedMilliseconds}');
       expect(true, true);
     });
     test("find 999999", () {
       Stopwatch stopwatch = new Stopwatch()..start();
       lista.find(999999);
       print(
-          'Find para un elemento al final de una lista de 1M de datos en ${stopwatch.elapsed}');
+          'Find para un elemento al final de una lista de 1M de datos en ${stopwatch.elapsedMilliseconds}');
       expect(true, true);
     });
     test("erase 0", () {
       Stopwatch stopwatch = new Stopwatch()..start();
       lista.erase(0);
       print(
-          'Erase para un elemento al inicio de una lista de 1M de datos en ${stopwatch.elapsed}');
+          'Erase para un elemento al inicio de una lista de 1M de datos en ${stopwatch.elapsedMilliseconds}');
       expect(true, true);
     });
     test("erase 499999", () {
       Stopwatch stopwatch = new Stopwatch()..start();
       lista.erase(499999);
       print(
-          'Erase para un elemento en el centro de una lista de 1M de datos en ${stopwatch.elapsed}');
+          'Erase para un elemento en el centro de una lista de 1M de datos en ${stopwatch.elapsedMilliseconds}');
       expect(true, true);
     });
     test("Erase 999999", () {
       Stopwatch stopwatch = new Stopwatch()..start();
       lista.erase(999999);
       print(
-          'Erase para un elemento al final de una lista de 1M de datos en ${stopwatch.elapsed}');
+          'Erase para un elemento al final de una lista de 1M de datos en ${stopwatch.elapsedMilliseconds}');
       expect(true, true);
     });
     test("add 0 en 0", () {
       Stopwatch stopwatch = new Stopwatch()..start();
       lista.add(0, 0);
       print(
-          'Agregar un elemento al inicio de una lista de 1M de datos en ${stopwatch.elapsed}');
+          'Agregar un elemento al inicio de una lista de 1M de datos en ${stopwatch.elapsedMilliseconds}');
       expect(true, true);
     });
     test("add 499999 en 499999", () {
       Stopwatch stopwatch = new Stopwatch()..start();
       lista.add(499999, 499999);
       print(
-          'Agregar un  elemento en el centro de una lista de 1M de datos en ${stopwatch.elapsed}');
+          'Agregar un  elemento en el centro de una lista de 1M de datos en ${stopwatch.elapsedMilliseconds}');
       expect(true, true);
     });
     test("add 999999 en 999999", () {
       Stopwatch stopwatch = new Stopwatch()..start();
       lista.add(999999, 999999);
       print(
-          'Agregar un  elemento al final de una lista de 1M de datos en ${stopwatch.elapsed}');
+          'Agregar un  elemento al final de una lista de 1M de datos en ${stopwatch.elapsedMilliseconds}');
       expect(true, true);
     });
   });
