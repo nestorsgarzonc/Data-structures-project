82d9b72e3f070357131a0a9808c6db77b7ce9718
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-11-17 10:25:41 -0500

    create service page created

--- lib/screens/freelancer/create_service_page.dart
+++ lib/screens/freelancer/create_service_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:waffly/widgets/appbar_with_backbutton_widget.dart';
 
 class CreateServicePage extends StatelessWidget {
   static const String route = 'CreateServicePage';
@@ -6,9 +7,24 @@ class CreateServicePage extends StatelessWidget {
   //TODO: finish form
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            const AppBarWithBackButtonWidget(text: 'Crea un servicio'),
+            Expanded(
+              child: ListView(
+                children: [
+                  const Text(
+                    'Hola,\nEstas a punto de crear un servicio',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
